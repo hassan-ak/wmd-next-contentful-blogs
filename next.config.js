fd9b89fdaf4a1560e68c9f548c9865cf0d4bc224
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2023-04-09 03:39:55 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  env: {
+    CONTENTFUL_SPACE_ID: "Enter Space-ID here",
+    CONTENTFUL_ACCESS_KEY: "Enter token heree"
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+        port: '',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
